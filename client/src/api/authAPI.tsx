325048087ee6938d729bae6dc4b508b1c93e32b6
Josh Kramer <kramer@nyu.edu>
2025-02-01 12:56:09 -0500

    pushing up test code for deployment

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,8 +1,27 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+    // TODONE: make a POST request to the login route
+
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    return await response.json(); 
+  } catch (error) {
+    console.error("Login error:", error);
+    return null;
+  }
+};
 
 
 
